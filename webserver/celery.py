244a8ef2d3976970f8647e5fdd3979932cebe6d7
Karl Gluck <karlgluck@gmail.com>
2015-01-17 15:57:30 -0600

    Remove debug task from Celery

--- webserver/celery.py
+++ webserver/celery.py
@@ -14,7 +14,3 @@ app = Celery('webserver')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
