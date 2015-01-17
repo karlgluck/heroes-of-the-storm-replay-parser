af0302151f7e31f6b424d4ac561b2cda762e31a0
Karl Gluck <karlgluck@gmail.com>
2015-01-17 15:48:25 -0600

    Debug API replay processing works with Celery tasks
    For the debug API, an uploaded file is saved on disk using Python's
    tempfile.NamedTemporaryFile with automatic deletion disabled. The
    name of this file is passed to a LocallyStoredReplayParsing Celery
    task. This task reads the file and returns the data to Celery, which
    stores it in Redis.
    
    The response to the user uploading a file is to reply with a URL
    to a results page. The GET parameter of the results page is the
    celery task ID, which can be used to look up the status and returned
    data from the Celery task. When processing completes, the JSON
    object on this page turns from status=PENDING to status=SUCCESS,
    with the processing results directly in the 'data' property.
    
    The production mode API will work slightly differently. Instead of
    storing data locally, the user will upload the file to S3 and
    let the server know it has done so. The Celery task launched for
    this type will access the file on S3, process it, and write the
    resulting JSON file back to S3. The SUCCESS response on the result
    page will be the address of the JSON file and not its contents.

--- api/tasks.py
+++ api/tasks.py
@@ -1,7 +1,36 @@
 from __future__ import absolute_import
 
+import os
+
 from celery import shared_task
+from celery.utils.log import get_task_logger
+
+
+log = get_task_logger(__name__)
 
 @shared_task
 def test(param):
     return 'The test task executed with argument "%s" ' % param
+
+from api.StormReplayParser import StormReplayParser
+
+@shared_task
+def LocallyStoredReplayParsingTask(fileName):
+    log.info('File name='+fileName)
+    replayFile = open(fileName)
+    srp = StormReplayParser(replayFile)
+    log.info("Created StormReplayParser, getting data") 
+    retval = {
+        'unique_match_id': srp.getUniqueMatchId(),
+        'map': srp.getMapName(),
+        'players': srp.getReplayPlayers(),
+        'chat': srp.getChat(),
+        #'game': srp.getReplayGameEvents(),
+    }
+    log.info("Finished reading from StormReplay. Cleaning up.")
+    replayFile.close()
+    os.remove(replayFile.name)
+    return retval
+
+# todo: task specific logging?
+# http://blog.mapado.com/task-specific-logging-in-celery/
